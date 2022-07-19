6fc99c20d4132c9c531d5383cb549d03af5fc91b
Mohamed Elbaz <mohamedelbazdev@gmail.com>
2022-07-19 20:56:05 +0200

    styling

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // database module
-var mysql = require('mysql');
+var mysql = require("mysql");
 var config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'aStore'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "aStore",
 };
 
 // init database
@@ -12,25 +12,25 @@ var pool = mysql.createPool(config);
 
 //Fetch data
 function RunQuery(sql, callback) {
-    pool.getConnection(function (err, conn) {
-        if (err) {
-            ShowErrors(err);
-        }
-        conn.query(sql, function (err, rows, fields) {
-            if (err) {
-                ShowErrors(err);
-            }
-            conn.release();
-            callback(rows);
-        });
+  pool.getConnection(function (err, conn) {
+    if (err) {
+      ShowErrors(err);
+    }
+    conn.query(sql, function (err, rows, fields) {
+      if (err) {
+        ShowErrors(err);
+      }
+      conn.release();
+      callback(rows);
     });
+  });
 }
 
 //Throw errors
 function ShowErrors(err) {
-    throw err;
+  throw err;
 }
 
 module.exports = {
-    RunQuery: RunQuery
+  RunQuery: RunQuery,
 };
